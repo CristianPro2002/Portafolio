a15519e66de34fba2a47638dafbb5703aaf37253
cristianyt2 <cristian.0713.cs@gmail.com>
2023-04-24 18:55:23 -0500

    styles

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,6 +1,14 @@
 import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Thumbs } from "swiper";
+import CardSlider from "./CardSlider";
+import imgAngular from "../assets/img/iconos/angular.png";
+import imgReact from "../assets/img/iconos/react.png";
+import imgDjango from "../assets/img/iconos/django.png";
+import imgPostgresql from "../assets/img/iconos/postgresql.png";
+import imgTailwindcss from "../assets/img/iconos/tailwindcss.png";
+import imgStyledComponents from "../assets/img/iconos/styledComponents.png";
+import imgCss from "../assets/img/iconos/css.png";
 import { effectView } from "../utils/interception";
 import "../styles/slider.css";
 
@@ -19,11 +27,96 @@ export default function Slider() {
   //   setResponsive(2);
   // }
 
+  const imagenesCard1 = [
+    {
+      name: "Angular",
+      imgRute: imgAngular,
+    },
+    {
+      name: "Django",
+      imgRute: imgDjango,
+    },
+    {
+      name: "Postgresql",
+      imgRute: imgPostgresql,
+    },
+    {
+      name: "Tailwindcss",
+      imgRute: imgTailwindcss,
+      especial: "img-tailwindcssCard",
+    },
+  ];
+
+  const dataCard1 = {
+    name: "Jesucristo Reina",
+    description: "Proyecto para la iglesia Jesucristo Reina",
+    url: "https://portfolioshopservices.shop/",
+    urlname: "portfolioshopservices.shop",
+    state: "En construcción...",
+  };
+
+  const imagenesCard2 = [
+    {
+      name: "React",
+      imgRute: imgReact,
+    },
+    {
+      name: "Css",
+      imgRute: imgCss,
+    }
+  ];
+
+  const dataCard2 = {
+    name: "Control de gastos",
+    description: "Proyecto para el control de gastos",
+    url: "https://control-gastos2023.netlify.app/",
+    urlname: "control-gastos2023.netlify.app",
+    state: "Terminado",
+  }
+
+  const imagenesCard3 = [
+    {
+      name: "React",
+      imgRute: imgReact,
+    },
+    {
+      name: "Tailwindcss",
+      imgRute: imgTailwindcss,
+      especial: "img-tailwindcssCard",
+    }
+  ]
+
+  const dataCard3 = {
+    name: "Control de citas",
+    description: "Proyecto para el control de citas veterinarias",
+    url: "https://control-citas2023.netlify.app/",
+    urlname: "control-citas2023.netlify.app",
+    state: "Terminado",
+  }
+
+  const imagenesCard4 = [
+    {
+      name: "React",
+      imgRute: imgReact,
+    },
+    {
+      name: "Styled-components",
+      imgRute: imgStyledComponents,
+    }
+  ]
+
+  const dataCard4 = {
+    name: "Cotizador de criptomonedas",
+    description: "Proyecto para cotizar criptomonedas",
+    url: "https://crypto-monedas.netlify.app/",
+    urlname: "crypto-monedas.netlify.app",
+  }
+
   return (
     <Swiper
       loop={true}
       spaceBetween={0}
-      slidesPerView={2}
+      slidesPerView={1}
       navigation={true}
       modules={[Navigation, Thumbs]}
       grabCursor={true}
@@ -32,29 +125,29 @@ export default function Slider() {
     >
       <SwiperSlide>
         <div style={{ display: "flex", justifyContent: "center" }}>
-          <div className="content-sliderCard" id="efectoPrueba1">
-            <h3>Jesucristo Reina1</h3>
+          <div className="content-sliderCard card1" id="efectoPrueba1">
+            <CardSlider imagenes={imagenesCard1} data={dataCard1} />
           </div>
         </div>
       </SwiperSlide>
       <SwiperSlide>
         <div style={{ display: "flex", justifyContent: "center" }}>
-          <div className="content-sliderCard" id="efectoPrueba2">
-            <h3>Jesucristo Reina2</h3>
+          <div className="content-sliderCard card2" id="efectoPrueba2">
+            <CardSlider imagenes={imagenesCard2} data={dataCard2} />
           </div>
         </div>
       </SwiperSlide>
       <SwiperSlide>
         <div style={{ display: "flex", justifyContent: "center" }}>
-          <div className="content-sliderCard" id="efectoPrueba3">
-            <h3>Jesucristo Reina3</h3>
+          <div className="content-sliderCard card3" id="efectoPrueba3">
+            <CardSlider imagenes={imagenesCard3} data={dataCard3} />
           </div>
         </div>
       </SwiperSlide>
       <SwiperSlide>
         <div style={{ display: "flex", justifyContent: "center" }}>
-          <div className="content-sliderCard" id="efectoPrueba4">
-            <h3>Jesucristo Reina4</h3>
+          <div className="content-sliderCard card4" id="efectoPrueba4">
+            <CardSlider imagenes={imagenesCard4} data={dataCard4} />
           </div>
         </div>
       </SwiperSlide>
