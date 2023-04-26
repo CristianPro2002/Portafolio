8ef7eac646a3c88cec49df2a45e65f4a342df814
cristianyt2 <cristian.0713.cs@gmail.com>
2023-04-26 17:05:37 -0500

    style

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Contacto from "../components/contacto";
+import Contacto from "../components/Contacto";
 import Proyectos from "../components/Proyectos";
 import Perfil from "../components/Perfil";
 import logoPerfil from "../assets/img/perfil.jpg";
